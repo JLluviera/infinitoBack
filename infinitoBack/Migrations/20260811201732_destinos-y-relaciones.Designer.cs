47d7debba01cf8194df16947823c3744279a4692
JLluviera <juanlluviera03@gmail.com>
2026-08-11 17:18:50 -0300

    Update-Database agrega destino y relacion

--- infinitoBack/Migrations/20260811201732_destinos-y-relaciones.Designer.cs
+++ infinitoBack/Migrations/20260811201732_destinos-y-relaciones.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using infinitoBack.Data;
 
@@ -11,9 +12,11 @@ using infinitoBack.Data;
 namespace infinitoBack.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260811201732_destinos-y-relaciones")]
+    partial class destinosyrelaciones
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
