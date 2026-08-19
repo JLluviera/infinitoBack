0f1ade5b139614ba5d782ffa7e0006bd60cb09c5
JLluviera <juanlluviera03@gmail.com>
2026-08-19 09:59:47 -0300

    Agrega excursionCrearDTO

--- infinitoBack/DTOs/ExcursionCrearDTO.cs
+++ infinitoBack/DTOs/ExcursionCrearDTO.cs
@@ -1,17 +1,16 @@
-﻿using infinitoBack.Models;
+﻿using infinitoBack.Enum;
 
 namespace infinitoBack.DTOs
 {
-    public class ExcursionCrearDTO
+    public class ExcursionCrearDto
     {
         public string Nombre { get; set; }
-
-        public int CantLugares { get; set; }
-
-        public int CantDias { get; set; }
-
-        public DateOnly FechaSalida { get; set; }
-
+        public decimal Precio { get; set; }
+        public decimal? Seña { get; set; }
+        public DateOnly FechaSalida { get; set; } 
+        public int DuracionDias { get; set; }
+        public string Descripcion { get; set; }
+        public TipoPaquete Tipo { get; set; }
         public int DestinoId { get; set; }
     }
 }
