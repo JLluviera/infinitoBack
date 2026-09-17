68ff195c274df8dff7583d2fbe655a0974c0f844
Gabriel Larrosa <gabriell7195@gmail.com>
2026-09-17 00:58:39 -0300

    Se completaron los endpoints de Transaccion

--- infinitoBack/Controllers/TransaccionController.cs
+++ infinitoBack/Controllers/TransaccionController.cs
@@ -22,13 +22,13 @@ namespace infinitoBack.Controllers
 
         public async Task<IActionResult> ObtenerTransacciones()
         {
-            List<Transaccion> Transacciones = await _context.Transacciones.ToListAsync();
-            return Ok(Transacciones);
+            List<Transaccion> transacciones = await _context.Transacciones.ToListAsync();
+            return Ok(transacciones);
 
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> ObtenerTransacciopnesPorId(int id)
+        public async Task<IActionResult> ObtenerTransaccionesPorId(int id)
         {
             Transaccion? transaccion = await _context.Transacciones.FindAsync(id);
             if (transaccion == null)
@@ -41,8 +41,54 @@ namespace infinitoBack.Controllers
 
         public async Task<IActionResult> CrearTransaccion([FromBody] TransaccionCrearDto transaccionDatos)
         {
-            bool existe = await _context.Transacciones.FirstAsync(transaccion=>transaccion.Id = transaccionDatos.Id)
-        } 
+            Transaccion transaccion = new Transaccion()
+            {
+                Monto = transaccionDatos.Monto,
+                FechaCreacion = transaccionDatos.FechaCreacion,
+                FormaDePAgo = transaccionDatos.FormaDePago,
+                Observaciones = transaccionDatos.Observaciones,
+                Estado = transaccionDatos.Estado,
+                IdReserva = transaccionDatos.IdReserva,
+                IdCliente = transaccionDatos.IdCliente,
+            };
 
+            await _context.Transacciones.AddAsync(transaccion);
+            await _context.SaveChangesAsync();
+            return Ok(transaccion);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditarTransaccion(int id, [FromBody] TransaccionCrearDto transaccionEditada)
+        {
+            Transaccion? transaccion = await _context.Transacciones.FindAsync(id);
+            if (transaccion == null)
+            {
+                return NotFound($"No se encontro ninguna transaccion con el id {id}");
+            }
+            transaccion.Monto = transaccionEditada.Monto;
+            transaccion.FechaCreacion = transaccionEditada.FechaCreacion;
+            transaccion.FormaDePAgo = transaccionEditada.FormaDePago;
+            transaccion.Observaciones = transaccionEditada.Observaciones;
+            transaccion.Estado = transaccionEditada.Estado;
+            transaccion.IdReserva = transaccionEditada.IdReserva;
+            transaccion.IdCliente = transaccionEditada.IdCliente;
+
+            await _context.SaveChangesAsync();
+            return Ok($"La transaccion con id {id} se actualizo correctamente");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> BorrarTransaccion(int id)
+        {
+            Transaccion? transaccion = await _context.Transacciones.FindAsync(id);
+            if (transaccion == null)
+            {
+                return NotFound($"No se encontro ninguna transaccion con id {id}");
+            }
+            _context.Transacciones.Remove(transaccion);
+            await _context.SaveChangesAsync();
+            return Ok($"La transaccion con el id {id} se borro correctamente");
+        }
     }
 }
