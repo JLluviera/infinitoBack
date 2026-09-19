34904ab4328d961936e4bcf0717cfe8414a004fb
Gabriel Larrosa <gabriell7195@gmail.com>
2026-09-19 09:17:56 -0300

    Se agrego transaccion en la base d edatos

---
+++ infinitoBack/Migrations/20260919121645_Se-creo-transaccion.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace infinitoBack.Migrations
+{
+    /// <inheritdoc />
+    public partial class Secreotransaccion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Transacciones",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Monto = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    FechaCreacion = table.Column<DateOnly>(type: "date", nullable: false),
+                    FormaDePAgo = table.Column<int>(type: "int", nullable: false),
+                    Observaciones = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<int>(type: "int", nullable: false),
+                    IdReserva = table.Column<int>(type: "int", nullable: false),
+                    ReservaId = table.Column<int>(type: "int", nullable: false),
+                    IdCliente = table.Column<int>(type: "int", nullable: false),
+                    ClienteId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transacciones", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transacciones_Clientes_ClienteId",
+                        column: x => x.ClienteId,
+                        principalTable: "Clientes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Transacciones_Reservas_ReservaId",
+                        column: x => x.ReservaId,
+                        principalTable: "Reservas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacciones_ClienteId",
+                table: "Transacciones",
+                column: "ClienteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transacciones_ReservaId",
+                table: "Transacciones",
+                column: "ReservaId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Transacciones");
+        }
+    }
+}
